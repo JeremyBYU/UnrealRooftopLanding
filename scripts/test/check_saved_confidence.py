268e61fda5a6c176865ae771fe1d43846d9acc03
Jeremy Castagno <jeremybyu@gmail.com>
2021-01-12 22:31:14 -0500

    Decently Fast Confidence Maps

--- scripts/test/check_saved_confidence.py
+++ scripts/test/check_saved_confidence.py
@@ -14,6 +14,9 @@ import yaml
 import quaternion
 from rich import print as rprint
 import numpy as np
+from scipy.interpolate import griddata
+from scipy.ndimage import uniform_filter
+from astropy.convolution import convolve, Box2DKernel
 from shapely.geometry import shape, Polygon
 import shapely
 from airsim.types import Vector3r, Quaternionr
@@ -77,7 +80,7 @@ def create_fake_confidence_map_seg(img_seg, seg2rgb_map, ds=4, roof_class=4):
                            order=0)
     classes = colors2class(img_seg_rgb_, seg2rgb_map).astype(np.float32)
     classes[classes != roof_class] = 0.0
-    classes[classes == roof_class] = 0.90
+    classes[classes == roof_class] = 1.0
 
     return classes
 
@@ -87,37 +90,46 @@ def create_confidence_map_planarity(tri_mesh, img_meta, airsim_settings, ds=4):
         (img_meta['height'], img_meta['width']), dtype=np.int) // ds
     img_meta['height'] = shape[0]
     img_meta['width'] = shape[1]
-
-    conf_map_plan = np.zeros(
-        (img_meta['height'], img_meta['width']), dtype=np.float32)
+    conf_map_plan = np.full(
+        (img_meta['height'], img_meta['width']), np.nan, dtype=np.float32)
+    box_kernel = Box2DKernel(3)
     t1 = time.perf_counter()
     triangles_np = np.asarray(tri_mesh.triangles)
     vertices_np = np.asarray(tri_mesh.vertices)
     triangle_normals_np = np.asarray(tri_mesh.triangle_normals)
     t2 = time.perf_counter()
-    triangle_centroids = vertices_np[triangles_np].mean(axis=1)
+    triangle_centroids = vertices_np[triangles_np[:, 0]]
+    # triangle_centroids = vertices_np[triangles_np].mean(axis=1) # slow, can be made faster
     t3 = time.perf_counter()
     triangles_planarity = triangle_normals_np @ np.array([[0], [0], [-1]])
     t4 = time.perf_counter()
     #
     pixels, mask = get_pixels_from_points(
-        triangle_centroids, img_meta, airsim_settings)
+        triangle_centroids, img_meta, airsim_settings) # slow
     t5 = time.perf_counter()
     triangles_planarity_filt = np.squeeze(triangles_planarity[mask, :])
     t6 = time.perf_counter()
     conf_map_plan[pixels[:, 1], pixels[:, 0]] = triangles_planarity_filt
+    conf_map_plan = convolve(conf_map_plan, box_kernel)
+    # conf_map_plan = uniform_filter(conf_map_plan, size=3)
+    # grid_x, grid_y = np.mgrid[0:img_meta['height'], 0:img_meta['width']]
+    # conf_map_plan = griddata(pixels, triangles_planarity_filt, (grid_y, grid_x), method='nearest')
     t7 = time.perf_counter()
 
     ms1 = (t2-t1) * 1000
-    ms2 = (t3-t2) * 1000
+    ms2 = (t3-t2) * 1000 # slow
     ms3 = (t4-t3) * 1000
-    ms4 = (t5-t4) * 1000
+    ms4 = (t5-t4) * 1000 # slow
     ms5 = (t6-t5) * 1000
     ms6 = (t7-t6) * 1000
     # print(ms1, ms2, ms3, ms4, ms5, ms6)
 
     return conf_map_plan
 
+
+def create_confidence_map_combined(seg_map, conf_map):
+    comb_map = (seg_map + conf_map) / 2.0
+    return comb_map
 
 def update_state(record, position='position', rotation='rotation'):
     # print("before")
@@ -168,21 +180,20 @@ def main(gui=True, segmented=False):
         pc_np = np.load(str(lidar_paths_dict[path_key]))
 
         conf_map_seg = create_fake_confidence_map_seg(img_seg, seg2rgb_map)
-        conf_map_seg_color = cm.viridis(
-            conf_map_seg)[:, :, :3][..., ::-1].copy()
 
         _, tri_mesh = extract_mesh(pc_np, config)
         t1 = time.perf_counter()
         conf_map_plan = create_confidence_map_planarity(
             tri_mesh, img_meta, airsim_settings)
         t2 = time.perf_counter()
-        print(t2-t1)
-        conf_map_plan_color = cm.viridis(conf_map_plan)[
-            :, :, :3][..., ::-1].copy()
+        conf_map_comb = create_confidence_map_combined(conf_map_seg, conf_map_plan)
 
         img_scene = np.concatenate((img_scene, img_seg), axis=1)
+        conf_map_seg_color = cm.viridis(conf_map_seg)[:, :, :3][..., ::-1].copy()
+        conf_map_plan_color = cm.viridis(conf_map_plan)[:, :, :3][..., ::-1].copy()
+        conf_map_comb_color = cm.viridis(conf_map_comb)[:, :, :3][..., ::-1].copy()
         img_conf = np.concatenate(
-            (conf_map_seg_color, conf_map_plan_color), axis=1)
+            (conf_map_seg_color, conf_map_plan_color, conf_map_comb_color), axis=1)
         cv2.imshow('Scene View', img_scene)
         cv2.imshow('Confidence View', img_conf)
         res = cv2.waitKey(0)
